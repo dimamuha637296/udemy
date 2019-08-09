44fc9abe1d8b323d76bb35263a959e87e873657d
Dmitriy <dima.muha637296@gmail.com>
2019-08-09 15:34:19 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+var a = 5;
+console.log(a);
+console.log("afasdasdad");
+// 
